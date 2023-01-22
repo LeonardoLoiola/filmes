61ba88ce9d872888cb5865fc69f7beabf748db50
Leonardo Loiola <leonardo.loiola.fernandes@gmail.com>
2023-01-22 18:53:46 -0300

    Inicio do processo de fazer o download dos arquivos

--- source/main.py
+++ source/main.py
@@ -12,3 +12,14 @@ class Checkmovies():
         
         ...
 
+    def donwload_html(self):
+        os.makedirs(self.DOWNLOAD_DATABASE, exist_ok= True)
+        # if not os.path.exists(self.DOWNLOAD_DATABASE):
+        #     os.mkdir(self.DOWNLOAD_DATABASE)
+        html_text = requests.get(self.URL_DATABASE).text
+        soup = BeautifulSoup(html_text, 'html.parser')
+        for link in soup.find_all('a'):
+            if 'gz' in link.get('href'):
+                print(link.get('href'))
+                wget.download(link.get('href'), out= self.DOWNLOAD_DATABASE)
+       
