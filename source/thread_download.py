762e083c901f05db5e515e1b825273a6c101fb56
Leonardo Loiola <leonardo.loiola.fernandes@gmail.com>
2023-01-22 19:28:35 -0300

    Transformar os downloads em um processo mult thread

---
+++ source/thread_download.py
@@ -0,0 +1,25 @@
+
+from threading import Thread
+import wget
+import time
+
+ini = time.time()
+class Multidownload(Thread):
+    def __init__(self, link, DOWNLOAD_DATABASE):
+        Thread.__init__(self)
+        self.link = link
+        self.DOWNLOAD_DATABASE = DOWNLOAD_DATABASE
+        ...
+
+    def run(self):
+
+        wget.download(self.link, out= self.DOWNLOAD_DATABASE)
+        ...
+
+# link = 'https://datasets.imdbws.com/name.basics.tsv.gz'
+# DOWNLOAD_DATABASE = "./"
+# a = Multidownload(link, DOWNLOAD_DATABASE)
+# a.start()
+# a.join()
+# fim = time.time()
+# print(fim - ini)
