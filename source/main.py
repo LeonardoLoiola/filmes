c6abab56314edb3d4070c3406b7f102b14388fb0
Leonardo Loiola <leonardo.loiola.fernandes@gmail.com>
2023-01-22 19:27:01 -0300

    Transformar os downloads em um processo mult thread

--- source/main.py
+++ source/main.py
@@ -1,8 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-import wget
+from thread_download import Multidownload
 import os
 import tarfile
+import time
 
 class Checkmovies():
     URL_DATABASE = "https://datasets.imdbws.com/"
@@ -14,12 +15,19 @@ class Checkmovies():
 
     def donwload_html(self):
         os.makedirs(self.DOWNLOAD_DATABASE, exist_ok= True)
+        lst_donwload = []
         # if not os.path.exists(self.DOWNLOAD_DATABASE):
         #     os.mkdir(self.DOWNLOAD_DATABASE)
         html_text = requests.get(self.URL_DATABASE).text
         soup = BeautifulSoup(html_text, 'html.parser')
         for link in soup.find_all('a'):
-            if 'gz' in link.get('href'):
-                print(link.get('href'))
-                wget.download(link.get('href'), out= self.DOWNLOAD_DATABASE)
-       
+            link = link.get('href')
+            if 'gz' in link:
+                lst_donwload.append(Multidownload(link, self.DOWNLOAD_DATABASE))
+                
+        for th in lst_donwload:
+            th.start()
+
+        for th in lst_donwload:
+            th.join()
+        print("aqui")
