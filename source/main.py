585a10581ac1c802d2adfb5e76fccb49dfd5ed17
Leonardo Loiola <leonardo.loiola.fernandes@gmail.com>
2023-01-22 20:14:08 -0300

    Extração dos arquivos

--- source/main.py
+++ source/main.py
@@ -2,13 +2,37 @@ import requests
 from bs4 import BeautifulSoup
 import wget
 import os
-import tarfile
+import gzip
+
 
 class Checkmovies():
     URL_DATABASE = "https://datasets.imdbws.com/"
     DOWNLOAD_DATABASE = "./download"
+    EXTRACT_DATABASE = "./extract"
 
     def __init__(self):
-        
+
         ...
 
+    def exctract_files(self):
+        os.makedirs(self.EXTRACT_DATABASE, exist_ok=True)
+        onlyfiles = [os.path.join(self.DOWNLOAD_DATABASE, f) for f in os.listdir(self.DOWNLOAD_DATABASE) if
+                     os.path.isfile(os.path.join(self.DOWNLOAD_DATABASE, f))]
+
+        for path in onlyfiles:
+
+            file = path.split('\\')[1]
+            file = file[: file.find('.gz')]
+            # my_tar = tarfile.open(file)
+            # my_tar.extractall(self.EXTRACT_DATABASE)
+            with gzip.open(path, 'rb') as f:
+                file_content = f.read() 
+                extract_file = os.path.join(self.EXTRACT_DATABASE, file) 
+                with open(extract_file, 'wb') as w:
+                    w.write(file_content)
+
+a = Checkmovies()
+a.exctract_files()
+# path = r"download\name.basics.tsv.gz"
+# path = path.split('\\')[1]
+# print()
