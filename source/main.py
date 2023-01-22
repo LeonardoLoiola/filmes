d45a9bb237d4c1d175ec2966a4cd9d606af108ba
Leonardo Loiola <leonardo.loiola.fernandes@gmail.com>
2023-01-22 18:49:52 -0300

    Primeiro commit do projeto. Criação inicial da classe de busca de filmes

---
+++ source/main.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+import wget
+import os
+import tarfile
+
+class Checkmovies():
+    URL_DATABASE = "https://datasets.imdbws.com/"
+    DOWNLOAD_DATABASE = "./download"
+
+    def __init__(self):
+        
+        ...
+
