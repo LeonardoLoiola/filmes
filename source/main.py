d5ab34667fba1ce08623e5465d54b77c2621b169
Leonardo Loiola <leonardo.loiola.fernandes@gmail.com>
2023-01-24 22:47:43 -0300

    Criação de todos os bancos de dados

--- source/main.py
+++ source/main.py
@@ -8,25 +8,24 @@ import shutil
 from datetime import date
 import gzip
 import pandas as pd
+from math import ceil
+
 class Checkmovies():
     URL_DATABASE = "https://datasets.imdbws.com/"
     DOWNLOAD_DATABASE = "./download"
+    EXTRACT_DATABASE = "./extract"
     D_COLUMNS = {
-        "title_basics": "(tconst,titleType,originalTitle,startYear,runtimeMinutes,genres)",
-        "a":"B
+        "title_basics": ('tconst', 'titleType', 'originalTitle', 'startYear', 'runtimeMinutes', 'genres'),
+        "title_crew": ('tconst', 'directors'),
+        "title_principals": ('tconst', 'nconst', 'job'),
+        "name_basics": ('nconst', 'primaryName', 'knownForTitles'),
+        "title_ratings": ('tconst', 'averageRating', 'numVotes'),
     }
-
-
-
-
-
-
-
-
 
     def __init__(self):
         self.atualizar = True
         self.extract = True
+        self.open_connection()
         ...
 
     def check_atualizacao(self):
@@ -46,7 +45,7 @@ class Checkmovies():
             m_ti = time.ctime(ti_m)
             t_obj = time.strptime(m_ti)
             T_stamp = time.strftime("%Y-%m-%d", t_obj)
-            if date.fromisoformat(T_stamp) != today :
+            if date.fromisoformat(T_stamp) != today:
                 shutil.rmtree(self.DOWNLOAD_DATABASE)
                 self.atualizar = True
                 return True
@@ -80,54 +79,107 @@ class Checkmovies():
         self.extract = True
         return True
 
-
     def exctract_files(self):
         if self.extract:
             shutil.rmtree(self.EXTRACT_DATABASE)
             os.makedirs(self.EXTRACT_DATABASE, exist_ok=True)
             onlyfiles = [os.path.join(self.DOWNLOAD_DATABASE, f) for f in os.listdir(self.DOWNLOAD_DATABASE) if
-                        os.path.isfile(os.path.join(self.DOWNLOAD_DATABASE, f))]
+                         os.path.isfile(os.path.join(self.DOWNLOAD_DATABASE, f))]
 
             for path in onlyfiles:
-                
+
                 file = path.split('\\')[1]
                 file = file[: file.find('.gz')]
                 # my_tar = tarfile.open(file)
                 # my_tar.extractall(self.EXTRACT_DATABASE)
                 with gzip.open(path, 'rb') as f:
-                    file_content = f.read() 
-                    extract_file = os.path.join(self.EXTRACT_DATABASE, file) 
+                    file_content = f.read()
+                    extract_file = os.path.join(self.EXTRACT_DATABASE, file)
                     with open(extract_file, 'wb') as w:
                         w.write(file_content)
-
 
     def open_connection(self):
         import mysql.connector
         import config
         self.conn = mysql.connector.connect(
-            host = config.d_keys['host'],
-            user = config.d_keys['user'],
-            password = config.d_keys['password'],
-            database = config.d_keys['database'])
-        
+            host=config.d_keys['host'],
+            user=config.d_keys['user'],
+            password=config.d_keys['password'],
+            database=config.d_keys['database'])
+
         self.cursor = self.conn.cursor()
 
     def close_connection(self):
         self.cursor.close()
         self.conn.close()
 
-
-    def base(self):
-        self.open_connection()
+    def id_movies(self):
         sql = """
         select tconst from title_basics
         """
         self.cursor.execute(sql)
         lst = self.cursor.fetchall()
-        df = pd.DataFrame(lst, columns=['tconst'])
-        print(df)
-        self.close_connection()
-    
-    # def commit(ini, final, cursor, conn, tuples):
-    #     sql = f"""INSERT INTO title_basics (tconst,titleType,originalTitle,startYear,runtimeMinutes,genres)
-    #                 VALUES {', '.join(tuples[ini:final])}"""
+        self.df_movies = pd.DataFrame(lst, columns=['tconst'])
+
+    def id_person(self):
+        sql = """
+        select DISTINCT nconst from title_principals
+        """
+        self.cursor.execute(sql)
+        lst = self.cursor.fetchall()
+        self.df_person = pd.DataFrame(lst, columns=['nconst'])
+
+
+    def create_database(self):
+        for key, value in self.D_COLUMNS.items():
+            df = pd.read_csv(os.path.join(self.EXTRACT_DATABASE, key.replace('_', '.') + '.tsv'),
+                             sep='\t',
+                             usecols=value)
+
+        
+            if key == "title_basics":
+                df = df[df.titleType == 'movie']
+                df = df.fillna("").drop(columns = "titleType")
+                
+                df.originalTitle = df.originalTitle.apply(lambda x: str(x)[:255])
+
+            elif key == "title_crew":
+                df = df[df.tconst.isin(self.df_movies.tconst)]
+                df.directors = df.directors.apply(lambda x: str(x)[:255])
+            
+            elif key == 'title_principals':
+                df = df[df.tconst.isin(self.df_movies.tconst)]
+
+            elif key == "name_basics":
+                df = df[df.nconst.isin(self.df_person.nconst)]
+                # df['knownForTitles'] = df.knownForTitles.apply(lambda x: str(x).replace("\\N","").split(','))
+
+            elif key == "title_ratings":
+                df = df[df.tconst.isin(self.df_movies.tconst)]
+                df.averageRating = df.averageRating.astype('float64')
+                df.numVotes = df.numVotes.astype('int64')
+
+            tuples = [str(tuple(x)) for x in df.to_numpy()]
+            self.commit(tuples, key, value)
+            del df, tuples
+
+
+    def commit(self, tuples, key, value):
+        print(key)
+        for val in range(0, len(tuples) + 1000, ceil(len(tuples)/100)):
+            if val == 0:
+                ini = val
+            else:
+                sql = f"""INSERT INTO {key} {str(value).replace("'","").replace("titleType,","")}
+                    VALUES {', '.join(tuples[ini:val])}"""
+                self.cursor.execute(sql)
+                self.conn.commit()
+                ini = val
+                print(f'Linhas subidas: {val}')
+        
+        if key == "title_basics":
+            self.id_movies()
+        elif key == "title_principals":
+            self.id_person()
+
+Checkmovies().create_database()
